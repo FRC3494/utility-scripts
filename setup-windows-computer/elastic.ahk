cab2d0185b0a005e29d0354463520999eaad3782
liujip0 <liujip2020@gmail.com>
2025-11-02 01:35:16 -0400

    make elastic install work

---
+++ setup-windows-computer/elastic.ahk
@@ -0,0 +1,26 @@
+#Requires AutoHotkey v2.0
+
+WinWait("Setup - Elastic ")
+WinActivate("Setup - Elastic ")
+WinWaitActive("Setup - Elastic ")
+
+WinGetClientPos &x, &y, &width, &height, "Setup - Elastic "
+; 698 x 532
+
+Click(width * 0.08, height * 0.81) ; I accept the agreement
+Sleep(500)
+Click(width * 0.78, height * 0.95) ; Next
+Sleep(1000)
+
+Click(width * 0.78, height * 0.95) ; Next
+Sleep(1000)
+
+Click(width * 0.78, height * 0.95) ; Install
+loop {
+    Sleep(1000)
+} until (PixelGetColor(width * 0.78, height * 0.95, "Setup - Elastic ") != 0xf0f0f0)
+
+Click(width * 0.40, height * 0.35) ; Launch Elastic
+Sleep(500)
+Click(width * 0.78, height * 0.95) ; Finish
+Sleep(1000)
--- setup-windows-computer/main.ps1
+++ setup-windows-computer/main.ps1
@@ -4,20 +4,28 @@ Add-Type -AssemblyName UIAutomationTypes
 
 $downloadFolder = "$env:USERPROFILE\Downloads\FRC-Setup"
 $wpilibVersion = "2025.3.2"
-
-$wshell = New-Object -ComObject wscript.shell
+$elasticVersion = "2025.2.2"
 
 function WPILib {
   $installUrl = "https://packages.wpilib.workers.dev/installer/v$wpilibVersion/Win64/WPILib_Windows-$wpilibVersion.iso"
-  # Invoke-WebRequest -Uri $installUrl -OutFile "$downloadFolder\WPILib_Windows-$wpilibVersion.iso"
+  Invoke-WebRequest -Uri $installUrl -OutFile "$downloadFolder\WPILib_Windows-$wpilibVersion.iso"
   $diskImage = Mount-DiskImage -ImagePath "$downloadFolder\WPILib_Windows-$wpilibVersion.iso" -PassThru
   $driveLetter = ($diskImage | Get-Volume).DriveLetter
 
   Start-Process -FilePath "$($driveLetter):\WPILibInstaller.exe"
   Start-Process -FilePath "$PSScriptRoot\wpilib.exe" -Wait
 
-  Start-Sleep -Seconds 5
+  Start-Sleep -Seconds 1
   Dismount-DiskImage -ImagePath "$downloadFolder\WPILib_Windows-$wpilibVersion.iso"
 }
 
-WPILib
+function Elastic {
+  $installUrl = "https://github.com/Gold872/elastic-dashboard/releases/download/v$elasticVersion/elastic-setup-windows.exe"
+  # Invoke-WebRequest -Uri $installUrl -OutFile "$downloadFolder\elastic-setup-windows.exe"
+
+  Start-Process -FilePath "$downloadFolder\elastic-setup-windows.exe"
+  Start-Process -FilePath "$PSScriptRoot\elastic.exe" -Wait
+}
+
+# WPILib
+Elastic
